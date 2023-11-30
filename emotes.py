d34c2da7df05923599434923f4fef6b414d57cb8
Baptiste Candellier <baptiste@candellier.me>
2023-11-30 19:35:04 +0100

    Download animated emotes

--- emotes.py
+++ emotes.py
@@ -7,20 +7,24 @@ import io
 
 import twitch
 from PIL import Image
+from PIL import ImageSequence
 
+client_id = os.environ["TWITCH_CLIENT_ID"]
+client_secret = os.environ["TWITCH_SECRET"]
 
-client_id = os.environ['TWITCH_CLIENT_ID']
-client_secret = os.environ['TWITCH_SECRET']
-
-margin = 24
-size = 512
+TARGET_MARGIN = 32
+TARGET_SIZE = 512
 
 
 def main(username: str, out_dir: str):
-    output_dir = out_dir + '/' + username.lower()
+    output_dir_raw = f"{out_dir}/{username.lower()}/raw"
+    output_dir_processed = f"{out_dir}/{username.lower()}/processed"
 
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
+    if not os.path.exists(output_dir_raw):
+        os.makedirs(output_dir_raw)
+
+    if not os.path.exists(output_dir_processed):
+        os.makedirs(output_dir_processed)
 
     helix = twitch.Helix(client_id, client_secret)
 
@@ -29,41 +33,69 @@ def main(username: str, out_dir: str):
 
     # Get emote list
     params = dict()
-    params['broadcaster_id'] = user.id
-    emotes = helix.api.get('chat/emotes', params)['data']
+    params["broadcaster_id"] = user.id
+    emotes = helix.api.get("chat/emotes", params)["data"]
+
+    # Example emote format:
+    # {
+    #  "id": "emotesv2_7ebfa7a173b549e3b906aef54d51c322",
+    #  "name": "htyKnuk",
+    #  "images": {
+    #    "url_1x": "https://static-cdn.jtvnw.net/emoticons/v2/emotesv2_7ebfa7a173b549e3b906aef54d51c322/static/light/1.0",
+    #    "url_2x": "https://static-cdn.jtvnw.net/emoticons/v2/emotesv2_7ebfa7a173b549e3b906aef54d51c322/static/light/2.0",
+    #    "url_4x": "https://static-cdn.jtvnw.net/emoticons/v2/emotesv2_7ebfa7a173b549e3b906aef54d51c322/static/light/3.0"
+    #  },
+    #  "tier": "1000",
+    #  "emote_type": "subscriptions",
+    #  "emote_set_id": "418849315",
+    #  "format": ["static", "animated"],
+    #  "scale": ["1.0", "2.0", "3.0"],
+    #  "theme_mode": ["light", "dark"]
+    # }
 
     # Download emotes
-    for emote in emotes:
-        print("downloading " + emote['name'])
-        images = emote['images']
-        if 'url_4x' in images:
-            emote_path = output_dir + '/' + emote['name'] + '.png'
-            if not os.path.exists(emote_path):
-                data = requests.get(images['url_4x'], allow_redirects=True)
+    for i, emote in enumerate(emotes):
+        is_animated = "animated" in emote["format"]
 
-                with open(emote_path, 'wb') as f:
-                    with Image.open(io.BytesIO(data.content)) as im:
-                        s = size - margin * 2
-                        add_margin(im.resize((s, s)), margin).save(f)
+        format = "animated" if is_animated else "static"
+        ext = "gif" if is_animated else "png"
+        url = f"https://static-cdn.jtvnw.net/emoticons/v2/{emote['id']}/{format}/light/3.0"
 
+        raw_path = f"{output_dir_raw}/{emote['name']}.{ext}"
 
-def add_margin(pil_img, margin):
-    width, height = pil_img.size
-    new_width = width + margin * 2
-    new_height = height + margin * 2
-    result = Image.new("RGBA", (new_width, new_height))
-    result.putalpha(0)
-    result.paste(pil_img, (margin, margin))
-    return result
+        print(f"Getting {i+1}/{len(emotes)}: {emote['name']} ({format})")
+
+        if not os.path.exists(raw_path):
+            print(f"  Downloading {url} -> {raw_path}")
+            data = requests.get(url, allow_redirects=True)
+
+            with open(raw_path, "wb") as f:
+                # Download to file
+                f.write(data.content)
+
+            print(f"  Saved to {raw_path}")
+
+        image_format = "apng" if is_animated else "png"
+        processed_path = f"{output_dir_processed}/{emote['name']}.{image_format}"
+        print(f"  Processing {raw_path} -> {processed_path}")
+
+        # Resize and add margin using ImageMagick
+        emote_size = TARGET_SIZE - TARGET_MARGIN * 2
+
+        os.system(
+            f"convert {raw_path} -format {image_format} -background transparent -gravity center -scale {emote_size}x{emote_size} -extent {TARGET_SIZE}x{TARGET_SIZE} {processed_path}"
+        )
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description='Download Twitch emotes to be used as Signal stickers.')
+        description="Download Twitch emotes to be used as Signal stickers."
+    )
 
-    parser.add_argument('username', help='Twitch username')
-    parser.add_argument('-o', dest='out_dir', default='./out',
-                        help='base output directory')
+    parser.add_argument("username", help="Twitch username")
+    parser.add_argument(
+        "-o", dest="out_dir", default="./out", help="base output directory"
+    )
 
     args = parser.parse_args()
     main(username=args.username, out_dir=args.out_dir)
